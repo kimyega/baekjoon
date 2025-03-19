8be945ebb662b4e9470f06622b6a033978b2bf63
kimyega <kimyega@naver.com>
2025-03-20 01:02:55 +0900

    1

--- src/main/java/test7287/Register.java
+++ src/main/java/test7287/Register.java
@@ -3,6 +3,7 @@ package test7287;
 
 public class Register {
     public static void main(String[] args) {
-        System.out.println("123\nYour_ICPC_Team_Name");
+
+        System.out.println("37\nkimyega");
     }
 }
