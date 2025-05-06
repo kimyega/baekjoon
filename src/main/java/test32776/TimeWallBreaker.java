ebd55c04a7b08a8d64fe6f23cba8ee72b7b4d512
kimyega <kimyega@naver.com>
2025-05-06 23:59:13 +0900

    baekjoon

--- src/main/java/test32776/TimeWallBreaker.java
+++ src/main/java/test32776/TimeWallBreaker.java
@@ -1,4 +1,25 @@
 package test32776;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class TimeWallBreaker {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int flight = 0;
+    int rail = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    while (st.hasMoreTokens()) {
+      flight += Integer.parseInt(st.nextToken());
+    }
+
+    String s = rail < flight ? "high speed rail" : "flight";
+    bw.write(s);
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
 }
