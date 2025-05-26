d37d06e2987cc338ef094c1d1949ac0ac05d5ba3
kimyega <kimyega@naver.com>
2025-05-26 16:32:41 +0900

    baekjoon

--- src/main/java/test32710/Gugudan.java
+++ src/main/java/test32710/Gugudan.java
@@ -1,19 +1,21 @@
 package test32710;
 
 import java.io.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
 
 public class Gugudan {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        boolean isGugudan = false;
+        AtomicBoolean isGugudan = new AtomicBoolean(false);
 
-        for (int i = 2; i <= 9; i++) {
-            if (n % i == 0 && n / i < 10 || n == 1) isGugudan = true;
-        }
+        IntStream.range(2, 10).forEach(i -> {
+            if (n % i == 0 && n / i < 10 || n == 1) isGugudan.set(true);
+        });
 
-        bw.write(isGugudan ? "1" : "0");
+        bw.write(isGugudan.get() ? "1" : "0");
 
         bw.flush();
         bw.close();
