badf2c12b30348fd4e0fed42120a91ed5222813c
kimyega <kimyega@naver.com>
2025-05-31 23:58:30 +0900

    baekjoon

---
+++ src/main/java/test13698/HawkEyes.java
@@ -0,0 +1,12 @@
+package test13698;
+
+import java.io.*;
+
+public class HawkEyes {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+  }
+}
