eab6f7648e0459165bc42e92f938226fceb0808e
kimyega <kimyega@naver.com>
2025-06-02 01:32:29 +0900

    baekjoon

--- src/main/java/test21312/EvenOddCocktail.java
+++ src/main/java/test21312/EvenOddCocktail.java
@@ -1,37 +1,44 @@
 package test21312;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class EvenOddCocktail {
 
   static class Cocktail {
-    List<Integer> even = new ArrayList<>();
+    List<Integer> all = new ArrayList<>();
     List<Integer> odd = new ArrayList<>();
 
-    Cocktail (int n) {
-      if (n % 2 == 0) {
-        this.even.add(n);
-      } else {
-        this.odd.add(n);
+    void add(int n) {
+      if (n % 2 != 0) {
+        odd.add(n);
       }
+
+      all.add(n);
     }
 
-
+    int getMax() {
+      Collections.sort(all, Collections.reverseOrder());
+      return odd.size() == 1 ? odd.get(0) : odd.size() == 2 ? odd.get(0) * odd.get(1) : all.get(0) * all.get(1) * all.get(2);
+    }
   }
 
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    Cocktail cocktail = new Cocktail();
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int a = Integer.parseInt(st.nextToken());
-    int b = Integer.parseInt(st.nextToken());
-    int c = Integer.parseInt(st.nextToken());
 
+    while (st.hasMoreTokens()) {
+      cocktail.add(Integer.parseInt(st.nextToken()));
+    }
 
+    bw.write(cocktail.getMax() + "");
+
+    bw.flush();
+    bw.close();
+    br.close();
   }
 }
