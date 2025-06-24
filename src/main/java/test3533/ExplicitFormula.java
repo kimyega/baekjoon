f18be6d9b1ffa262e7dfe1b0003e723396a42ea3
kimyega <kimyega@naver.com>
2025-06-24 17:11:41 +0900

    baekjoon

---
+++ src/main/java/test3533/ExplicitFormula.java
@@ -0,0 +1,12 @@
+package test3533;
+
+import java.io.*;
+
+public class ExplicitFormula {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
