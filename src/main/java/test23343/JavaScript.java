1446c00b7eaaed7704356667e5ed2909f8b3f837
kimyega <kimyega@naver.com>
2025-06-25 11:36:59 +0900

    baekjoon

---
+++ src/main/java/test23343/JavaScript.java
@@ -0,0 +1,18 @@
+package test23343;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class JavaScript {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+
+
+    }
+}
