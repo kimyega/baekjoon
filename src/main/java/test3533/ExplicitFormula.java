a424f1022f75bf4f95a9c4b24d30e0281945024d
kimyega <kimyega@naver.com>
2025-06-25 11:36:35 +0900

    baekjoon

--- src/main/java/test3533/ExplicitFormula.java
+++ src/main/java/test3533/ExplicitFormula.java
@@ -1,12 +1,36 @@
 package test3533;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ExplicitFormula {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] x = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
+        int pairCount = IntStream.range(0, 9)
+                .flatMap(i -> IntStream.range(i + 1, 10)
+                        .filter(j -> x[i] == 1 || x[j] == 1)
+                )
+                .map(j -> 1)
+                .sum();
+
+        int tripletCount = IntStream.range(0, 8)
+                .flatMap(i -> IntStream.range(i + 1, 9)
+                        .flatMap(j -> IntStream.range(j + 1, 10)
+                                .filter(k -> x[i] == 1 || x[j] == 1 || x[k] == 1)))
+                .map(k -> 1)
+                .sum();
+
+        bw.write((pairCount + tripletCount) % 2 + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
