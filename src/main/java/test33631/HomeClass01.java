c5992a0af3b41d6d419f7bf694ef54105c1a822a
kimyega <kimyega@naver.com>
2025-07-14 19:59:14 +0900

    baekjoon

---
+++ src/main/java/test33631/HomeClass01.java
@@ -0,0 +1,12 @@
+package test33631;
+
+import java.io.*;
+
+public class HomeClass01 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+  }
+}
