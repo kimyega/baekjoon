a49248bf150d3c48af1d845d96243018dd026da8
kimyega <kimyega@naver.com>
2025-07-29 23:57:06 +0900

    baekjoon

---
+++ src/main/java/test9773/IDKey.java
@@ -0,0 +1,36 @@
+package test9773;
+
+import java.io.*;
+
+public class IDKey {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    StringBuilder sb = new StringBuilder();
+
+    while (n-- > 0) {
+      String s = br.readLine();
+      sb.append(makeIdKey(s).substring(1)).append("\n");
+    }
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  static String makeIdKey(String s) {
+    int sum = 0;
+    for (char c : s.toCharArray()) {
+      sum += c - '0';
+    }
+
+    int last = Integer.parseInt(s.substring(s.length() - 3));
+    sum += last * 10;
+
+    return sum / 10000 == 0 ? sum + 10000 + "" : sum % 100000 + "";
+  }
+}
