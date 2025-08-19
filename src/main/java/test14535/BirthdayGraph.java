1058ad25287f11cbd5fe4d184684b745418327d3
kimyega <kimyega@naver.com>
2025-08-19 17:38:28 +0900

    baekjoon

---
+++ src/main/java/test14535/BirthdayGraph.java
@@ -0,0 +1,12 @@
+package test14535;
+
+import java.io.*;
+
+public class BirthdayGraph {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
