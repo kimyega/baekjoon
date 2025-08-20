57012eb532724e685f00ff2c0f74b4e66f29d4f3
kimyega <kimyega@naver.com>
2025-08-20 15:12:06 +0900

    baekjoon

---
+++ src/main/java/test32969/ConferenceApplication.java
@@ -0,0 +1,36 @@
+package test32969;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class ConferenceApplication {
+
+    static class Conference {
+        String title;
+        String[] digitalKeywords = {"social", "history", "language", "literacy"};
+        String[] publicKeywords = {"bigdata", "public", "society"};
+
+        Conference(String title) {
+            this.title = title;
+        }
+        public String classifyCategory() {
+            boolean digitalFound = Arrays.stream(digitalKeywords).anyMatch(title::contains);
+            boolean publicFound = Arrays.stream(publicKeywords).anyMatch(title::contains);
+            return digitalFound ? "digital humanities" : publicFound ? "public bigdata" : "";
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String title = br.readLine();
+
+        Conference conference = new Conference(title);
+        bw.write(conference.classifyCategory());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
