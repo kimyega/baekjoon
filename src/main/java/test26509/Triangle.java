2aa1a9133c2c6a7747cf5f0c0940894c684d695f
kimyega <kimyega@naver.com>
2025-09-02 09:34:41 +0900

    baekjoon

---
+++ src/main/java/test26509/Triangle.java
@@ -0,0 +1,12 @@
+package test26509;
+
+import java.io.*;
+
+public class Triangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
