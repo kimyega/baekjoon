fedb28f86513c9fe8d0ac4105836e886f6c7888b
kimyega <kimyega@naver.com>
2025-09-02 16:41:10 +0900

    baekjoon

---
+++ src/main/java/test21167/CurveSpeed.java
@@ -0,0 +1,12 @@
+package test21167;
+
+import java.io.*;
+
+public class CurveSpeed {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+}
