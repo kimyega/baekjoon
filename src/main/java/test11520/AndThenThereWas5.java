7fb06d249bf285f143279dd24741f92d77cd31be
kimyega <kimyega@naver.com>
2025-09-04 17:25:01 +0900

    baekjoon

---
+++ src/main/java/test11520/AndThenThereWas5.java
@@ -0,0 +1,22 @@
+package test11520;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class AndThenThereWas5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        double PI = Math.PI;
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+        }
+    }
+}
